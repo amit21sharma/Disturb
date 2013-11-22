45c2182b6267a2d7d70d0371e76f907ab1217e32
U-dell-PC\dell <dell@dell-PC.(none)>
2013-11-22 19:32:27 +0530

    disturb code till verification of sms

---
+++ Disturb/src/com/example/disturb/Splash.java
@@ -0,0 +1,44 @@
+package com.example.disturb;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+
+public class Splash extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+	Thread timer= new Thread() {
+		public void run(){
+			try{
+				sleep(2500);
+			}catch(InterruptedException e){ 
+				e.printStackTrace();
+			}
+		finally{ 
+			Intent In = new Intent("com.example.disturb.MAINACTIVITY");
+			startActivity(In);
+		}
+	}
+	};
+	timer.start();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		finish();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.splash, menu);
+		return true;
+	}
+
+}
